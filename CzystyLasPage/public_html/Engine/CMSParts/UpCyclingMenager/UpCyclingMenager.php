c505865610fbde9648aac13bcbe09cfa019b91ec
ukidabek <ukidabek@gmail.com>
2014-10-29 19:23:44 +0100

    Finalizacja "W lesie" i upcyklingu.

--- CzystyLasPage/public_html/Engine/CMSParts/UpCyclingMenager/UpCyclingMenager.php
+++ CzystyLasPage/public_html/Engine/CMSParts/UpCyclingMenager/UpCyclingMenager.php
@@ -7,18 +7,18 @@
  */
 
 /**
- * Description of InForestMenager
+ * Description of UpCyclingMenager
  *
  * @author Lukasz
  */
-class InForestMenager extends DatabaseEditor {
-
+class UpCyclingMenager extends DatabaseEditor {
+    
     public function __construct($_colNames = array(), $_target) {
         parent::__construct($_colNames, $_target, 'inforest');
 
-        $this->DeleteButton = '<button type="submit" name="function" value="delete_inforest" class="newsDeleteItem">-</button>';
-        $this->AddButton = '<button type="submit" name="function" value="add_inforest" class="newsOK">OK</button>';
-        $this->EditButton = '<button type="submit" name="function" value="edit_inforest" class="newsOK">OK</button>';
+        $this->DeleteButton = '<button type="submit" name="function" value="delete_upcycling" class="newsDeleteItem">-</button>';
+        $this->AddButton = '<button type="submit" name="function" value="add_upcycling" class="newsOK">OK</button>';
+        $this->EditButton = '<button type="submit" name="function" value="edit_upcycling" class="newsOK">OK</button>';
     }
 
     public function Add($_params = array()) {
@@ -28,33 +28,33 @@ class InForestMenager extends DatabaseEditor {
         move_uploaded_file($_params[0], "Data/Images/" . $_params[3]);
 
         $id = mysql_insert_id();
-        $q = "INSERT INTO `czysty-las-database`.`inforest` (`Id`, `Photo`, `Title`, `Description`) VALUES (NULL, '$id', '$_params[1]', '$_params[2]')";
+        $q = "INSERT INTO `czysty-las-database`.`upcycling` (`Id`, `Photo`, `Title`, `Description`) VALUES (NULL, '$id', '$_params[1]', '$_params[2]')";
         $ins = mysql_query($q);
     }
 
     public function Delete($_param) {
-        $q = "SELECT `Photo` FROM `inforest` WHERE `Id` = " . $_param;
-        $ins1 = mysql_query($q);
-
-        $photoId = mysql_fetch_array($ins1)['Photo'];
-
-        $q = "SELECT `Name` FROM `images` WHERE `Id` = " . $photoId;
-        $ins1 = mysql_query($q);
-
-        $photo = "./Data/Images/" . mysql_fetch_array($ins1)['Name'];
-        echo $photo;
-        unlink($photo);
-
-        $q = "DELETE FROM `czysty-las-database`.`images` WHERE `images`.`Id` = " . $photoId;
-        $ins = mysql_query($q);
-
-
-        $q = "DELETE FROM `czysty-las-database`.`inforest` WHERE `inforest`.`Id` = " . $_param;
-        $ins = mysql_query($q);
+       $q = "SELECT `Photo` FROM `upcycling` WHERE `Id` = ".$_param; 
+       $ins1 = mysql_query($q);
+       
+       $photoId = mysql_fetch_array($ins1)['Photo'];
+       
+       $q = "SELECT `Name` FROM `images` WHERE `Id` = ".$photoId;
+       $ins1 = mysql_query($q);
+       
+       $photo = "./Data/Images/" .mysql_fetch_array($ins1)['Name'];
+       unlink($photo);
+       
+       $q = "DELETE FROM `czysty-las-database`.`images` WHERE `images`.`Id` = ".$photoId;
+       $ins = mysql_query($q);
+       
+       
+       $q = "DELETE FROM `czysty-las-database`.`upcycling` WHERE `upcycling`.`Id` = ".$_param;
+       $ins = mysql_query($q);        
     }
 
     public function Edit($_params = array()) {
-        $q = "SELECT `Photo` FROM `inforest` WHERE `Id` = " . $_params[0];
+        
+        $q = "SELECT `Photo` FROM `upcycling` WHERE `Id` = " . $_params[0];
         $ins1 = mysql_query($q);
 
         $q = "SELECT `Name` FROM `images` WHERE `Id` = " . mysql_fetch_array($ins1)['Photo'];
@@ -70,13 +70,13 @@ class InForestMenager extends DatabaseEditor {
 
             move_uploaded_file($_params[1]['tmp_name'], "Data/Images/" . $_params[1]['name']);
             //    echo 'YES!';
-            echo $_params[1]['name'];
+
             $q = "INSERT INTO `czysty-las-database`.`images` (`Id`, `Name`, `GalleryId`) VALUES (NULL, '" . $_params[1]['name'] . "', '0')";
             $ins = mysql_query($q);
 
-            $q = "UPDATE `czysty-las-database`.`inforest` SET `Photo` = '" . mysql_insert_id() . "', `Title` = '$_params[2]', `Description` = '$_params[3]' WHERE `inforest`.`Id` = " . $_params[0];
+            $q = "UPDATE `czysty-las-database`.`upcycling` SET `Photo` = '" . mysql_insert_id() . "', `Title` = '$_params[2]', `Description` = '$_params[3]' WHERE `upcycling`.`Id` = " . $_params[0];
         } else {
-            $q = "UPDATE `czysty-las-database`.`inforest` SET `Title` = '$_params[2]', `Description` = '$_params[3]' WHERE `inforest`.`Id` = " . $_params[0];
+            $q = "UPDATE `czysty-las-database`.`upcycling` SET `Title` = '$_params[2]', `Description` = '$_params[3]' WHERE `upcycling`.`Id` = " . $_params[0];
         }
 
         $ins = mysql_query($q);
@@ -85,10 +85,10 @@ class InForestMenager extends DatabaseEditor {
     public function Show() {
 
         if (!isset($_GET['add']) && !isset($_GET['Id'])) {
-            $q = "SELECT * FROM `inforest`";
+            $q = "SELECT * FROM `upcycling`";
             $ins = mysql_query($q);
             echo '<div class="inforestContent">';
-            echo '<a class="inforestAddItem" href="CMS.php?function=inforest&add=true">Dodaj</a>';
+            echo '<a class="inforestAddItem" href="CMS.php?function=upcycling&add=true">Dodaj</a>';
 
             while ($print = mysql_fetch_array($ins)) {
                 $q = "SELECT * FROM `images` WHERE `Id` = " . $print[$this->ColsNames[1]];
@@ -96,7 +96,7 @@ class InForestMenager extends DatabaseEditor {
                 $print1 = mysql_fetch_array($ins1);
 
 
-                echo '<a class="inforestItem" href="CMS.php?function=inforest&Id=' . $print['Id'] . '">';
+                echo '<a class="inforestItem" href="CMS.php?function=upcycling&Id=' . $print['Id'] . '">';
                 echo '<div class="inforestImageHolder">';
                 echo '<form action="' . $this->Target . '" method="post">'
                 . '<input type="text" hidden="true" name="' . $this->ColsNames[0] . '"  value="' . $print[$this->ColsNames[0]] . '"/>'; //  Przekazanie Id elementu do usunięcia przez post -> powrót do CMS.php -> Wywolanie odpowiedniej metody
@@ -130,7 +130,7 @@ class InForestMenager extends DatabaseEditor {
                 <div class="newsCenter">' . $this->AddButton . '<a class="newsOK" href="CMS.php?function=inforest">Powrót</a></div>
                 </form>';
             } else {
-                $q = "SELECT * FROM `inforest` WHERE `Id` = " . $id;
+                $q = "SELECT * FROM `upcycling` WHERE `Id` = " . $id;
                 $Edit = mysql_query($q);
                 $EditPrint = mysql_fetch_array($Edit);
                 echo '
@@ -145,5 +145,4 @@ class InForestMenager extends DatabaseEditor {
             }
         }
     }
-
 }
